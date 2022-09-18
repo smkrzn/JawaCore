b51235ffb4a2e08df8f2c1f5dc2ef75635ddb160
smkrzn <smk.forwork@gmail.com>
2022-09-18 19:25:47 +0300

    LДЗ 4

---
+++ src/Lesson_4/Main.java
@@ -0,0 +1,42 @@
+package Lesson_4;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] myArray = {"World", "Boll", "Aplle", "Roud", "Men", "Boll", "Run", "Sun", "Star", "Men", "Sea", "Men", "Cat",
+                "Lime", "Sun", "Moon", "World"};
+
+        ArrayList<String> myArrayList;
+        HashSet<String> uniqueName;
+        HashMap<String,Integer> repeat;
+        Processing processing = new Processing();
+        myArrayList = processing.arrayToList(myArray);
+        uniqueName = processing.uniqueName(myArrayList);
+        System.out.println(uniqueName);
+        repeat = processing.repeat(myArrayList,uniqueName);
+        for (Map.Entry<String,Integer> pair: repeat.entrySet()) {
+            System.out.println(pair.getKey() + " повторов: " + pair.getValue());
+        }
+
+        Telefon_Numbers myDirectory = new Telefon_Numbers();
+        myDirectory.add("Mike", 79009999920l);
+        myDirectory.add("Elen", 79009209920l);
+        myDirectory.add("Pit", 79009985920l);
+        myDirectory.add("Mike", 79009563420l);
+        myDirectory.add("Jane", 79009913762l);
+        myDirectory.add("Sam", 79009998634l);
+        myDirectory.add("Kate", 79009346770l);
+        myDirectory.add("Pit", 79005559920l);
+
+        String name = "Mike";
+        HashMap<String, HashSet<Long>> result = new HashMap<>();
+        myDirectory.get(name);
+
+    }
+}
---
+++ src/Lesson_4/Processing.java
@@ -0,0 +1,39 @@
+package Lesson_4;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Processing <T extends String>{
+
+    public ArrayList<String> arrayToList (String[] array){
+        ArrayList<String> newArrayList = new ArrayList<>(array.length);
+        for (int i = 0; i < array.length; i ++){
+            newArrayList.add(array[i]);
+        }
+        return newArrayList;
+    }
+
+    public HashSet<String> uniqueName (ArrayList<String> arrayList){
+        HashSet<String> uniqueName = new HashSet<>();
+        for (String a: arrayList){
+            uniqueName.add(a);
+        }
+        return uniqueName;
+    }
+
+    public HashMap<String,Integer> repeat(ArrayList<String> generalList, HashSet<String> uniqueList){
+        HashMap<String,Integer> repeat = new HashMap<>();
+        for (String b: uniqueList){
+            Integer i = 0;
+            for (String c: generalList){
+                if(b==c){
+                    i++;
+                }
+            }
+            repeat.put(b,i);
+        }
+        return repeat;
+    }
+}
